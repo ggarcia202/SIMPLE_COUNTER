599cb18501b4003076b14f062eab6b8871f5110e
German Garcia <germangergarcia@gmail.com>
2025-11-27 12:44:37 +0100

    update

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,18 +1,47 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-//Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap"
-
-// index.css'
 import '../styles/index.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
-// components
-import Home from './components/Home';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+function SimpleCounter(props){
+  return (
+    <div className='bigCounter'>
+      <div className='calendar'>
+        <FontAwesomeIcon icon={faClock} />
+      </div>
+      <div className='four'>{props.digitFour}</div>
+      <div className='trhee'>{props.digitThree}</div>
+      <div className='two'>{props.digitTwo}</div>
+      <div className='one'>{props.digitOne}</div>
+    </div>
+  );
+}
+
+
+let counter = 0;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+setInterval(()=>{
+  counter++
+  const digitOne = counter % 10;
+  const digitTwo = Math.floor(counter / 10) % 10;
+  const digitThree = Math.floor(counter / 100) % 10;
+  const digitFour = Math.floor(counter / 1000) % 10;
+  
+  root.render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    <SimpleCounter
+    digitOne={digitOne}
+    digitTwo={digitTwo}
+    digitThree={digitThree}
+    digitFour={digitFour}
+    />
+  </React.StrictMode>
+);
+},1000);
